234ad4cc4ee917e8eb8b9e256ed6ca8b4867e697
Anh3h <courageangeh@gmail.com>
2019-05-10 23:24:27 -0400

    Test topic create and update actions at the API level

--- src/test/java/com/courage/library/unitTest/controller/TopicControllerTest.java
+++ src/test/java/com/courage/library/unitTest/controller/TopicControllerTest.java
@@ -1,10 +1,14 @@
 package com.courage.library.unitTest.controller;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.UUID;
+
 import com.courage.library.controller.TopicController;
+import com.courage.library.factory.JsonConverter;
 import com.courage.library.factory.TopicFactory;
 import com.courage.library.model.Topic;
 import com.courage.library.service.command.TopicCommand;
@@ -33,6 +37,46 @@ public class TopicControllerTest {
 
 	@MockBean
 	private TopicCommand topicCommand;
+
+	@Test
+	public void createTopicRequest_returnHttp201AndCreatedTopic() throws Exception {
+		Topic topic = TopicFactory.instance();
+		given(this.topicCommand.createTopic(any(Topic.class))).willReturn(topic);
+
+		this.mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/topics")
+				.content(JsonConverter.toJSON(topic))
+				.accept(MediaType.APPLICATION_JSON)
+				.contentType(MediaType.APPLICATION_JSON))
+			.andExpect(status().isCreated())
+			.andExpect(jsonPath("name").value(topic.getName()));
+	}
+
+	@Test
+	public void updateTopicRequest_returnHttp200AndUpdatedTopic() throws Exception {
+		Topic topic = TopicFactory.instance();
+		given(this.topicCommand.updateTopic(any(Topic.class))).willReturn(topic);
+
+		this.mockMvc.perform(MockMvcRequestBuilders.put("/api/v1/topics/" + topic.getId())
+				.content(JsonConverter.toJSON(topic))
+				.accept(MediaType.APPLICATION_JSON)
+				.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("name").value(topic.getName()));
+	}
+
+	@Test
+	public void invalidUpdateTopicRequest_returnHttp400() throws Exception {
+		Topic topic = TopicFactory.instance();
+		String id = UUID.randomUUID().toString();
+		given(this.topicCommand.updateTopic(any(Topic.class))).willReturn(topic);
+
+		this.mockMvc.perform(MockMvcRequestBuilders.put("/api/v1/topics/" + id)
+				.content(JsonConverter.toJSON(topic))
+				.accept(MediaType.APPLICATION_JSON)
+				.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isBadRequest());
+	}
+
 
 	@Test
 	public void getTopicsRequest_returnsHttp200AndAListOfExistingTopics() throws Exception {
