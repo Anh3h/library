4b78ab052dd69dfef727134021028b1105d6ae1b
Anh3h <courageangeh@gmail.com>
2019-05-13 23:56:45 -0400

    Test create user at the API level

---
+++ src/test/java/com/courage/library/unitTest/controller/UserControllerTest.java
@@ -0,0 +1,54 @@
+package com.courage.library.unitTest.controller;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.courage.library.controller.UserController;
+import com.courage.library.factory.JsonConverter;
+import com.courage.library.factory.UserFactory;
+import com.courage.library.model.User;
+import com.courage.library.model.dto.UserDTO;
+import com.courage.library.service.command.UserCommand;
+import com.courage.library.service.query.UserQuery;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(value = UserController.class, secure = false)
+public class UserControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@MockBean
+	private UserCommand userCommand;
+
+	@MockBean
+	private UserQuery userQuery;
+
+	@Test
+	public void createUserRequest_returnsHttp201AndCreatedUser() throws Exception {
+		User user = UserFactory.instance();
+		UserDTO userDTO = new UserDTO(user.getId(), user.getFirstName(), user.getLastName(),
+				user.getUsername(), user.getEmail(), user.getPassword(),
+				user.getDob(), user.getTelephone(), user.getRole().getId());
+		given(this.userCommand.createUser(any(UserDTO.class))).willReturn(user);
+
+		this.mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/users")
+				.accept(MediaType.APPLICATION_JSON_VALUE)
+				.contentType(MediaType.APPLICATION_JSON_VALUE)
+				.content(JsonConverter.toJSON(userDTO)))
+			.andExpect(status().isCreated())
+			.andExpect(jsonPath("email").value(user.getEmail()));
+	}
+
+}
