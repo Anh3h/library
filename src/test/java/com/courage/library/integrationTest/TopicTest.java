0aa660bd525510aa80c7958833fcf67770cba33a
Anh3h <courageangeh@gmail.com>
2019-06-04 00:27:02 -0400

    Fix bug in get topics integration test

--- src/test/java/com/courage/library/integrationTest/TopicTest.java
+++ src/test/java/com/courage/library/integrationTest/TopicTest.java
@@ -88,9 +88,10 @@ public class TopicTest {
 
 		ResponseEntity<String> response = this.restTemplate.exchange(baseUrl, HttpMethod.GET, entity,
 				String.class);
+		String numberOfElts = JsonPath.parse(response.getBody()).read(".numberOfElements").toString();
 
 		assertThat(response.getStatusCode()).isEqualByComparingTo(HttpStatus.OK);
-		assertThat(new Integer(JsonPath.parse(response.getBody()).read(".totalElements").toString()))
+		assertThat(new Integer(numberOfElts.substring(1, numberOfElts.length()-1)))
 			.isGreaterThan(3);
 	}
 
