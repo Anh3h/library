c1f0c3c161c6cd0d780263712ad60f37245c4ee7
Anh3h <courageangeh@gmail.com>
2019-06-05 17:33:13 -0400

    Implement integration test for create book.

---
+++ src/test/java/com/courage/library/integrationTest/BookTest.java
@@ -0,0 +1,67 @@
+package com.courage.library.integrationTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.courage.library.factory.BookFactory;
+import com.courage.library.factory.JsonConverter;
+import com.courage.library.factory.TopicFactory;
+import com.courage.library.model.Book;
+import com.courage.library.model.Topic;
+import com.courage.library.model.dto.BookDTO;
+import com.jayway.jsonpath.JsonPath;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class BookTest {
+
+	@LocalServerPort
+	private String port;
+	private String baseUrl = "http://localhost:";
+	private TestRestTemplate restTemplate = new TestRestTemplate();
+	private HttpHeaders httpHeaders = new HttpHeaders();
+
+	@Before
+	public void before() {
+		baseUrl += port;
+		httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+		httpHeaders.set("Authorization", "Bearer" + Authenticate.getAccessToken(baseUrl));
+		baseUrl += "/api/v1/books";
+	}
+
+	private ResponseEntity<String> createTopic(Topic topic) {
+		String url = "http://localhost:" + port + "/api/v1/topics";
+		HttpEntity entity = new HttpEntity(JsonConverter.toJSON(topic), httpHeaders);
+
+		return this.restTemplate.exchange(url, HttpMethod.POST, entity, String.class);
+	}
+
+	@Test
+	public void testCreateBook() {
+		Book book = BookFactory.instance();
+		String topicId = JsonPath.parse(this.createTopic(book.getTopic()).getBody()).read("id").toString();
+		book.getTopic().setId(topicId);
+		BookDTO bookDTO = BookFactory.convertToDTO(book);
+		HttpEntity entity = new HttpEntity(JsonConverter.toJSON(bookDTO), httpHeaders);
+
+		ResponseEntity<String> response = this.restTemplate.exchange(baseUrl, HttpMethod.POST,
+				entity, String.class);
+
+		assertThat(response.getStatusCode()).isEqualByComparingTo(HttpStatus.CREATED);
+		assertThat(JsonPath.parse(response.getBody()).read("title").toString())
+				.isEqualTo(book.getTitle());
+	}
+
+}
