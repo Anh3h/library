691a3add1d5cd23af9d5c1e3e220e9d988b2d926
Anh3h <courageangeh@gmail.com>
2019-06-16 23:33:18 -0400

    Implement integration test to test update comment

--- src/test/java/com/courage/library/integrationTest/CommentTest.java
+++ src/test/java/com/courage/library/integrationTest/CommentTest.java
@@ -2,11 +2,14 @@ package com.courage.library.integrationTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.UUID;
+
 import com.courage.library.factory.CommentFactory;
 import com.courage.library.factory.JsonConverter;
 import com.courage.library.model.Comment;
 import com.courage.library.model.dto.CommentDTO;
 import com.jayway.jsonpath.JsonPath;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.http.HttpResponse;
 import org.junit.Before;
 import org.junit.Test;
@@ -40,9 +43,7 @@ public class CommentTest {
 		baseUrl += "/api/v1/comments";
 	}
 
-	@Test
-	public void testCreateComment() {
-		Comment comment = CommentFactory.instance();
+	private ResponseEntity<String> createComment(Comment comment) {
 		ResponseEntity<String> bookResponse = Helper.createBook(port, comment.getBook());
 		ResponseEntity<String> userResponse = Helper.createUser(port, comment.getUser());
 		comment.getBook().setId(JsonPath.parse(bookResponse.getBody()).read("id").toString());
@@ -51,10 +52,69 @@ public class CommentTest {
 		CommentDTO commentDTO = CommentFactory.convertToDTO(comment);
 		HttpEntity entity = new HttpEntity(JsonConverter.toJSON(commentDTO), httpHeaders);
 
-		ResponseEntity<String> response = this.restTemplate.exchange(baseUrl, HttpMethod.POST, entity,
+		return this.restTemplate.exchange(baseUrl, HttpMethod.POST, entity,
 				String.class);
-
-		assertThat(response.getStatusCode()).isEqualByComparingTo(HttpStatus.CREATED);
 	}
 
+	@Test
+	public void testCreateComment() {
+		Comment comment = CommentFactory.instance();
+
+		ResponseEntity<String> response = this.createComment(comment);
+
+		assertThat(response.getStatusCode()).isEqualByComparingTo(HttpStatus.CREATED);
+		assertThat(JsonPath.parse(response.getBody()).read("text").toString())
+				.isEqualTo(comment.getText());
+		assertThat(JsonPath.parse(response.getBody()).read("user.id").toString())
+				.isEqualTo(comment.getUser().getId());
+		assertThat(JsonPath.parse(response.getBody()).read("book.id").toString())
+				.isEqualTo(comment.getBook().getId());
+	}
+
+	@Test
+	public void testUpdateComment() {
+		Comment comment = CommentFactory.instance();
+		ResponseEntity<String> commentRes = this.createComment(comment);
+		comment.setId(JsonPath.parse(commentRes.getBody()).read("id").toString());
+		comment.getUser().setId(JsonPath.parse(commentRes.getBody()).read("user.id").toString());
+		comment.getBook().setId(JsonPath.parse(commentRes.getBody()).read("book.id").toString());
+		CommentDTO commentDTO = CommentFactory.convertToDTO(comment);
+		HttpEntity entity = new HttpEntity(JsonConverter.toJSON(commentDTO), httpHeaders);
+		String url = baseUrl + "/" + comment.getId();
+
+		ResponseEntity<String> response = this.restTemplate.exchange(url, HttpMethod.PUT, entity,
+				String.class);
+
+		assertThat(response.getStatusCode()).isEqualByComparingTo(HttpStatus.OK);
+		assertThat(JsonPath.parse(response.getBody()).read("text").toString())
+				.isEqualTo(comment.getText());
+		assertThat(JsonPath.parse(response.getBody()).read("user.id").toString())
+				.isEqualTo(comment.getUser().getId());
+		assertThat(JsonPath.parse(response.getBody()).read("book.id").toString())
+				.isEqualTo(comment.getBook().getId());
+	}
+
+	@Test
+	public void testInValidUpdateComment() {
+		Comment comment = CommentFactory.instance();
+		HttpEntity<String> entity = new HttpEntity(JsonConverter.toJSON(comment), httpHeaders);
+		String url = baseUrl + "/" + UUID.randomUUID().toString();
+
+		ResponseEntity<String> response = this.restTemplate.exchange(url, HttpMethod.PUT, entity,
+				String.class);
+
+		assertThat(response.getStatusCode()).isEqualByComparingTo(HttpStatus.BAD_REQUEST);
+	}
+
+	@Test
+	public void testUpdateNotExistingComment() {
+		Comment comment = CommentFactory.instance();
+		HttpEntity<String> entity = new HttpEntity(JsonConverter.toJSON(comment), httpHeaders);
+		String url = baseUrl + "/" + comment.getId();
+
+		ResponseEntity<String> response = this.restTemplate.exchange(url, HttpMethod.PUT, entity,
+				String.class);
+
+		assertThat(response.getStatusCode()).isEqualByComparingTo(HttpStatus.NOT_FOUND);
+	}
 }
