67f7ff4c40c6789d32b32eccda6a008b586b4424
Anh3h <courageangeh@gmail.com>
2019-12-12 10:24:39 -0500

    Fix bug in sql initialization script

--- library.sql
+++ library.sql
@@ -37,7 +37,7 @@ CREATE TABLE IF NOT EXISTS `book` (
 
 INSERT INTO `book` (`id`, `author`, `available_qty`, `down_votes`, `edition`, `isbn`, `num_of_borrows`, `publication_date`, `publisher`, `shelf`, `title`, `total_qty`, `up_votes`, `topic_id`, `cover_image`) VALUES
 ('0fb6f11d-34cc-4dd5-8d45-5ad751ebb7ab', 'K. N. King', 1, 0, '', '', 2, '2018-07-16 23:11:19', 'CreateSpace Independent', 'Level 1 B1', 'C Programming: A Modern Approach', 1, 1, '921ad0c0-f6bd-428e-ba38-6bb00384bec6', 'https://www.googleapis.com/download/storage/v1/b/e-library/o/41%2BbFKtFHjL-2018-07-31-112522018.jpg?generation=1533036323459983&alt=media'),
-('185b6639-16d2-47d2-b09c-6a805c09b764', 'Dietel', 0, -1, '', '0133811905', 0, '2014-02-17 05:26:36', 'O\'RIEILLY', 'Level 1 B1', 'Java™ How To Program', 0, 1, '921ad0c0-f6bd-428e-ba38-6bb00384bec6', 'https://www.googleapis.com/download/storage/v1/b/e-library/o/java-HTP-2018-08-09-105002484.jpg?generation=1533811808828088&alt=media'),
+('185b6639-16d2-47d2-b09c-6a805c09b764', 'Dietel', 0, -1, '', '0133811905', 0, '2014-02-17 05:26:36', "O'RIEILLY", 'Level 1 B1', 'Java™ How To Program', 0, 1, '921ad0c0-f6bd-428e-ba38-6bb00384bec6', 'https://www.googleapis.com/download/storage/v1/b/e-library/o/java-HTP-2018-08-09-105002484.jpg?generation=1533811808828088&alt=media'),
 ('cc675a91-e392-4bf9-a0a6-4d1eef7134d2', '', 1, 0, '', '56780986789', 2, '2018-07-31 13:06:17', '', 'Level 2 B1', 'Digital marketing', 1, 0, '921ad0c0-f6bd-428e-ba38-6bb00384bec6', NULL),
 ('ba4df098-16a1-4448-805e-3c7379caa389', 'Ed Wright', 3, 0, 'Kindle Edition', '12345256', 0, NULL, '', 'Level 2 b3', 'Positive Thinking - The Key to Success', 3, 0, 'a85eea19-3012-480d-98a7-47221948d908', 'https://www.googleapis.com/download/storage/v1/b/e-library/o/positive-thinkx-2018-08-09-110157581.jpg?generation=1533812519398880&alt=media'),
 ('002bc556-fd02-4118-b919-578e322cf427', ' W. Chan Kim and Renée Mauborgne', 3, 0, '', '123453573', 0, '2017-09-26 01:00:00', '', 'Level 1 b3', 'Blue Ocean Shift: Beyond Competing', 3, 0, 'fdf995d5-8993-4db0-88ee-f5a5befc1fd2', 'https://www.googleapis.com/download/storage/v1/b/e-library/o/marketing-2018-08-09-121907157.jpg?generation=1533817150160278&alt=media');
@@ -208,12 +208,12 @@ CREATE TABLE IF NOT EXISTS `user` (
 --
 
 INSERT INTO `user` (`id`, `dob`, `email`, `first_name`, `last_name`, `password`, `telephone`, `username`, `role_id`) VALUES
-('7115c1f7-c96c-4268-9d37-6e5650bce07c', '2018-08-21 01:00:00', 'admin@gmail.com', 'Main', 'Admin', '$2a$10$fU8WnHtUxeiZD4IfvvPW5eudP9DSqg6Nom2p/zq20LMRLCY3BRXpG', '', 'admin', 'cb3e2b7d-34fa-4d47-b35e-d82d8689eddb'),
-('61991d73-b5b9-4e2e-9df9-5f3d96df2f4e', '2018-07-16 22:23:00', 'janedoe@mail.com', 'Jane', 'Doe', '$2a$10$mhBAjwnLHzZcOGB3IUsciOfeJ8JEDED0EwHcH6n6IJjd7JNw/gIVK', '', 'janedoe', 'b3553bc8-f57e-4385-beea-cdb492ef90bc'),
-('2939d03b-d94f-4442-8d31-c5ebd2fbc009', '2018-07-16 22:32:05', 'mikemill@mail.com', 'Mike', 'Mill', '$2a$10$aInoRlyPTvkI0RuRnylNxeiN99g8jG1QCyUMWov5RlGM.GT0FUHkK', '', 'mike-mill', 'cb3e2b7d-34fa-4d47-b35e-d82d8689eddb'),
-('533bf99b-5cd7-433a-9a3d-47be3ee5f55f', '2018-07-31 12:20:24', 'john@mail.com', 'John', 'Doe', '$2a$10$x8letcKXvevNi74VbAMtFuwHKepfPwBnlYDkMwTyFoUkJy4.Bk7Na', '', '', 'cb3e2b7d-34fa-4d47-b35e-d82d8689eddb'),
-('d82f6483-79ce-4edb-8177-507224279699', NULL, 'mike-mill@mail.cm', 'Mike', 'Mill', '$2a$10$yrNt.VG.wk85mIuPFl9BXeMmQO3HVNTcba0.lU.tOXPZCtTdD/9Va', NULL, 'Mike Mill', 'cb3e2b7d-34fa-4d47-b35e-d82d8689eddb'),
-('ed49695e-3d1f-4064-bc60-6cd6af969005', NULL, 'mileymill@mail.cm', 'Miley', 'mill', '$2a$10$Iv/bReypykLb3uEmqiXvGekLqLFMzfC1r9VN18OIcwtcCtFtFWy.6', NULL, 'Miley mill', 'cb3e2b7d-34fa-4d47-b35e-d82d8689eddb');
+('7115c1f7-c96c-4268-9d37-6e5650bce07c', '2018-08-21 01:00:00', 'admin@gmail.com', 'Main', 'Admin', '{bcrypt}$2a$10$fU8WnHtUxeiZD4IfvvPW5eudP9DSqg6Nom2p/zq20LMRLCY3BRXpG', '', 'admin', 'cb3e2b7d-34fa-4d47-b35e-d82d8689eddb'),
+('61991d73-b5b9-4e2e-9df9-5f3d96df2f4e', '2018-07-16 22:23:00', 'janedoe@mail.com', 'Jane', 'Doe', '{bcrypt}$2a$10$mhBAjwnLHzZcOGB3IUsciOfeJ8JEDED0EwHcH6n6IJjd7JNw/gIVK', '', 'janedoe', 'b3553bc8-f57e-4385-beea-cdb492ef90bc'),
+('2939d03b-d94f-4442-8d31-c5ebd2fbc009', '2018-07-16 22:32:05', 'mikemill@mail.com', 'Mike', 'Mill', '{bcrypt}$2a$10$aInoRlyPTvkI0RuRnylNxeiN99g8jG1QCyUMWov5RlGM.GT0FUHkK', '', 'mike-mill', 'cb3e2b7d-34fa-4d47-b35e-d82d8689eddb'),
+('533bf99b-5cd7-433a-9a3d-47be3ee5f55f', '2018-07-31 12:20:24', 'john@mail.com', 'John', 'Doe', '{bcrypt}$2a$10$x8letcKXvevNi74VbAMtFuwHKepfPwBnlYDkMwTyFoUkJy4.Bk7Na', '', '', 'cb3e2b7d-34fa-4d47-b35e-d82d8689eddb'),
+('d82f6483-79ce-4edb-8177-507224279699', NULL, 'mike-mill@mail.cm', 'Mike', 'Mill', '{bcrypt}$2a$10$yrNt.VG.wk85mIuPFl9BXeMmQO3HVNTcba0.lU.tOXPZCtTdD/9Va', NULL, 'Mike Mill', 'cb3e2b7d-34fa-4d47-b35e-d82d8689eddb'),
+('ed49695e-3d1f-4064-bc60-6cd6af969005', NULL, 'mileymill@mail.cm', 'Miley', 'mill', '{bcrypt}$2a$10$Iv/bReypykLb3uEmqiXvGekLqLFMzfC1r9VN18OIcwtcCtFtFWy.6', NULL, 'Miley mill', 'cb3e2b7d-34fa-4d47-b35e-d82d8689eddb');
 
 --
 -- Indexes for dumped tables
